4045da59efb2761c17a93687d78b0c64963062e1
meansoup <alsrnr1210@gmail.com>
2017-05-20 15:48:54 +0900

    POST img to server

---
+++ app/src/main/java/com/tzutalin/dlibtest/Communication/FileUploadService.java
@@ -0,0 +1,19 @@
+package com.tzutalin.dlibtest.Communication;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+/**
+ * Created by alsrn on 2017-05-20.
+ */
+
+public interface FileUploadService {
+    @Multipart
+    @POST("/selfie/identify")
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("name") RequestBody name);
+}
--- app/src/main/java/com/tzutalin/dlibtest/Communication/Manipulation_setting.java
+++ app/src/main/java/com/tzutalin/dlibtest/Communication/Manipulation_setting.java
@@ -8,6 +8,14 @@ import android.widget.Toast;
 
 import com.tzutalin.dlibtest.R;
 
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -111,5 +119,38 @@ public class Manipulation_setting extends Activity {
                 Log.e("Not Response", t.getLocalizedMessage());
             }
         });
+    }
+
+    public void uploadFile(String filePath) {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        FileUploadService service = new Retrofit.Builder().baseUrl("http://ec2-52-78-198-113.ap-northeast-2.compute.amazonaws.com").client(client).build().create(FileUploadService.class);
+
+        File file = new File(filePath);         //Log.d(filePath, file.toString());
+
+        RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), reqFile);
+        //MultipartBody.Part.create(reqFile);
+        RequestBody name = RequestBody.create(MediaType.parse("text/plain"), "upload_test");
+
+        retrofit2.Call<okhttp3.ResponseBody> req = service.postImage(body, name);
+        req.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                Toast.makeText(getApplicationContext(), "change url appropriately", Toast.LENGTH_SHORT).show();
+                if (response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "response code: " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
     }
 }
